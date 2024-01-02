19eec796e59536b99f925d7048d4632015ff320b
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-02 23:00:52 +0100

    😁 Task9 ES6 method properties

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,15 +1,11 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: (income) => {
-      return `$${income}`;
-    },
-    getIncomeInEuros: (income) => {
-      return `${income} euros`;
-    },
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 
   return fullBudget;
