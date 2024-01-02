d38b2994a3b7e4f9c717826bfa141c4e3f690060
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-02 17:12:58 +0100

    😥 Task1 eslint-disable no-unused-vars

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    /* eslint-disable no-unused-vars */
     const task = true;
     const task2 = false;
-    return [task, task2];
   }
 
   return [task, task2];
