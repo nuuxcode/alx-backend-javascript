84cb0585e26b3deeabd23ad5cf7f75423d831a8c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-04 14:44:46 +0100

    😆 Task2 A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,64 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._checkType(name, 'string', 'Name');
+    this._checkType(length, 'number', 'Length');
+    this._checkType(students, 'array', 'Students');
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._checkType(name, 'string', 'Name');
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    this._checkType(length, 'number', 'Length');
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    this._checkType(students, 'array', 'Students');
+    students.forEach(student => this._checkType(student, 'string', 'Students'));
+    this._students = students;
+  }
+
+  _checkType(value, type, variableName, errorMessage) {
+    if (!errorMessage) {
+      switch (type) {
+        case 'string':
+          errorMessage = `${variableName} must be a string`;
+          break;
+        case 'number':
+          errorMessage = `${variableName} must be a number`;
+          break;
+        case 'array':
+          errorMessage = `${variableName} must be an array of strings`;
+          break;
+        default:
+          errorMessage = `Invalid type`;
+      }
+    }
+
+    if (type === 'array') {
+      if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
+        throw new TypeError(errorMessage);
+      }
+    } else if (typeof value !== type) {
+      throw new TypeError(errorMessage);
+    }
+  }
+}
