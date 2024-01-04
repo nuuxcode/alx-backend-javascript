1e0e22e978c0d14f30d0dcef9c0cb40e6c89f2c6
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-04 23:16:05 +0100

    😵 Task5 fix esling

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,7 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+    if (new.target !== Building
+      && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
