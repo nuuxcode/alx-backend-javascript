da3541f7cde28bbddffb224aaf6d1a29400d6950
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-04 20:24:51 +0100

    🙃 Task5 A Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+export default class Building {
+  constructor(sqft) {
+    if (new.target !== Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+}
