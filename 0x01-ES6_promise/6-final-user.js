bc8daeef6e13e7ae58086fda9c21ba31736f9e6e
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-04 01:33:54 +0100

    😫 Task6 ops i just get it

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,6 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, photo) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(photo)])
-    .then((values) => values)
+    .then((results) => results.map(({ status, value, reason }) => ({
+      status,
+      value: status === 'fulfilled' ? value : reason,
+    })))
     .catch(() => console.log('Signup system offline'));
 }
