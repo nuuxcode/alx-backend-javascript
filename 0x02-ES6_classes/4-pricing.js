d307343dcc93e8e4e7e5b6eaf44b59e9c757979f
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-04 19:57:10 +0100

    😎 Task4 Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
