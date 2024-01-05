8c943e22d70400d791033ff4136ea872f036e089
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-05 02:07:09 +0100

    🎁 Task11 EVAR

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,12 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
