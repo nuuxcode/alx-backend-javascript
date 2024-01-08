81afdc1a23d60cebb12aeb5752984459c1367f49
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-08 16:35:12 +0100

    🎈🎇🧨 Task8 Fix ESLINT

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(xSet, xStartString) {
   return Array.from(xSet)
-    .filter(ele => ele.startsWith(xStartString) && xStartString.length > 0)
-    .map(ele => ele.replace(xStartString, ''))
+    .filter((ele) => ele.startsWith(xStartString) && xStartString.length > 0)
+    .map((ele) => ele.replace(xStartString, ''))
     .join('-');
 }
--- 0x03-ES6_data_manipulation/8-main.js
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -1,4 +1,6 @@
 import cleanSet from "./8-clean_set.js";
 
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set([]), 'bon'));
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
+console.log(cleanSet(new Set([]), ''));
