06af671dd928e43ce145a072619b80bf415684dd
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-08 16:30:01 +0100

    🎀🎁🎀 Task0 fix ESLINT

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,6 +2,6 @@ export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
