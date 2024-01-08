f8f5fcd5646c8dc6132d724b0e89e4e24b346461
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-08 17:18:00 +0100

    🎨👔🧥 Task5 Fix ESLINT

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -5,5 +5,4 @@ export default function createInt8TypedArray(length, position, value) {
   const xint8Array = new Int8Array(length);
   xint8Array.fill(value, position, position + 1);
   return new DataView(xint8Array.buffer);
-
 }
