a7dec5ac430c2e821c4113d09018a3942aef5770
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-08 16:46:36 +0100

    🎁🧶🧵 Task5 add throw error in case out of range

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
   const xint8Array = new Int8Array(length);
   xint8Array.fill(value, position, position + 1);
   return new DataView(xint8Array.buffer);
+
 }
