cd0e47c34a3a60a985529b2c48f48fa5c1ade101
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-08 16:31:20 +0100

    🎀🎁🎀 Task3 fix ESLINT

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(listStudents) {
-  return listStudents.reduce((xacc, { id }) => xacc + id, 0)
+  return listStudents.reduce((xacc, { id }) => xacc + id, 0);
 }
