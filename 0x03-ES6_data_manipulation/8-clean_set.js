09e343d398914405e27c088252698fb5005eb55a
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-08 17:39:19 +0100

    🧧🎡🛒 Task8 add case of undefined check

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(xSet, xStartString) {
+  if (!xSet || !xStartString) {
+    return '';
+  }
   return Array.from(xSet)
     .filter((ele) => ele.startsWith(xStartString) && xStartString.length > 0)
     .map((ele) => ele.replace(xStartString, ''))
--- 0x03-ES6_data_manipulation/8-main.js
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -1,6 +1,8 @@
 import cleanSet from "./8-clean_set.js";
 
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
-console.log(cleanSet(new Set([]), 'bon'));
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
 console.log(cleanSet(new Set([]), ''));
+console.log(cleanSet());
+console.log(cleanSet(new Set(['bonjovi']), 'bon'));
+console.log(cleanSet(new Set(['bonbon']), 'bon'));
