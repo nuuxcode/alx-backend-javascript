fb28975c4081a73361c30851b905a6067d8717dd
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-09 09:49:38 +0100

    🤑😜😝 Task10 add Error

--- 0x03-ES6_data_manipulation/10-main.js
+++ 0x03-ES6_data_manipulation/10-main.js
@@ -4,5 +4,5 @@ import groceriesList from "./9-groceries_list.js";
 const map = groceriesList();
 console.log(map);
 
-updateUniqueItems(map)
+updateUniqueItems("")
 console.log(map);
--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,5 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
   map.forEach((v, k) => {
     if (v === 1) {
       map.set(k, 100);
