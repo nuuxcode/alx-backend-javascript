74b3215fe4cccebb2d5c4868710198edba88ddc9
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-02-26 10:44:43 +0100

    😋😍🥰 Task2 Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const readline = require('readline');
+
+function countStudents(path) {
+  fs.access(path, fs.constants.F_OK, (err) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+  });
+  const fileStream = fs.createReadStream(path);
+  const rl = readline.createInterface({
+    input: fileStream,
+    output: process.stdout,
+    terminal: false,
+  });
+  const hashtable = {};
+  let students = -1;
+  rl.on('line', (line) => {
+    const colums = line.split(',');
+    const field = colums[3];
+    const firstname = colums[0];
+    if (students >= 0) {
+      if (!Object.hasOwnProperty.call(hashtable, field)) {
+        hashtable[field] = [];
+      }
+      hashtable[field] = [...hashtable[field], firstname];
+    }
+    students += 1;
+  });
+  rl.on('close', () => {
+    console.log(`Number of students: ${students}`);
+    for (const key in hashtable) {
+      if (Object.hasOwnProperty.call(hashtable, key)) {
+        const students = [];
+        hashtable[key].forEach((value) => {
+          students.push(value);
+        });
+        console.log(`Number of students in ${key}: ${students.length}. List: ${students.join(', ')}`);
+      }
+    }
+  });
+}
+module.exports = countStudents;
