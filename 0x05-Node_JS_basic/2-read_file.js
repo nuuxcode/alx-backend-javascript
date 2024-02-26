9b1c07982323712a880465cb09dc1c4fe20abe19
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-02-26 21:41:19 +0100

    😛😥🤩 Task2 seperate error

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,38 +2,41 @@ const fs = require('fs');
 const readline = require('readline');
 
 function countStudents(path) {
-  fs.access(path, fs.constants.F_OK, (err) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
+  const stream = fs.createReadStream(path);
+  stream.on('error', () => {
+    throw new Error('Cannot load the database');
   });
-  const rl = readline.createInterface({
-    input: fs.createReadStream(path),
-    output: process.stdout,
-    terminal: false,
-  });
-  const hashtable = {};
-  let students = -1;
-  rl.on('line', (line) => {
-    const colums = line.split(',');
-    const field = colums[3];
-    const firstname = colums[0];
-    if (students >= 0) {
-      if (!Object.hasOwnProperty.call(hashtable, field)) {
-        hashtable[field] = [];
+  try {
+    const rl = readline.createInterface({
+      input: stream,
+      output: process.stdout,
+      terminal: false,
+    });
+    const hashtable = {};
+    let students = -1;
+    rl.on('line', (line) => {
+      const colums = line.split(',');
+      const field = colums[3];
+      const firstname = colums[0];
+      if (students >= 0) {
+        if (!Object.hasOwnProperty.call(hashtable, field)) {
+          hashtable[field] = [];
+        }
+        hashtable[field] = [...hashtable[field], firstname];
       }
-      hashtable[field] = [...hashtable[field], firstname];
-    }
-    students += 1;
-  });
-  rl.on('close', () => {
-    console.log(`Number of students: ${students}`);
-    for (const key in hashtable) {
-      if (Object.hasOwnProperty.call(hashtable, key)) {
-        console.log(`Number of students in ${key}: ${hashtable[key].length}. List: ${hashtable[key].join(', ')}`);
+      students += 1;
+    });
+    rl.on('close', () => {
+      console.log(`Number of students: ${students}`);
+      for (const key in hashtable) {
+        if (Object.hasOwnProperty.call(hashtable, key)) {
+          console.log(`Number of students in ${key}: ${hashtable[key].length}. List: ${hashtable[key].join(', ')}`);
+        }
       }
-    }
-  });
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
