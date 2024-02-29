73b2dc2a028ed5b4fa495954f66a3064f863a853
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-02-29 14:55:34 +0100

    😊😁😑 Task0 more test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,15 +4,7 @@ const calculateNumber = require('./0-calcul.js');
 describe('calculateNumber', () => {
 
 
-  it('should return -3 when inputs are -1.5 and -2.5', () => {
-    assert.strictEqual(calculateNumber(-1.5, -2.5), -3);
-  });
 
-
-  // int float
-  it('should return 4 when inputs are 1 and 2.7', () => {
-    assert.strictEqual(calculateNumber(1, 2.7), 4);
-  });
 
   // float int
   it('should return 4 when inputs are 1.7 and 2', () => {
