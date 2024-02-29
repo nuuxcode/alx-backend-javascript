b50baab4b7305be9ceafe33f19b0fc3c45f895b6
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-02-29 14:54:40 +0100

    😊😁😀 Task0 more test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,19 +3,33 @@ const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
 
-  // int int
-  it('should return 4 when inputs are 2 and 2', () => {
-    assert.strictEqual(calculateNumber(2, 2), 4);
-  });
 
-  // float float
-  it('should return 5 when inputs are 1.5 and 2.5', () => {
-    assert.strictEqual(calculateNumber(1.5, 2.5), 5);
-  });
   it('should return -3 when inputs are -1.5 and -2.5', () => {
     assert.strictEqual(calculateNumber(-1.5, -2.5), -3);
   });
 
+
+  // int float
+  it('should return 4 when inputs are 1 and 2.7', () => {
+    assert.strictEqual(calculateNumber(1, 2.7), 4);
+  });
+
+  // float int
+  it('should return 4 when inputs are 1.7 and 2', () => {
+    assert.strictEqual(calculateNumber(1.7, 2), 4);
+  });
+
+  // all in negative
+  it('should return -4 when inputs are -2 and -2', () => {
+    assert.strictEqual(calculateNumber(-2, -2), -4);
+  });
+
+  it('should return -4 when inputs are -1 and -2.7', () => {
+    assert.strictEqual(calculateNumber(-1, -2.7), -4);
+  });
+  it('should return -4 when inputs are -1.7 and -2', () => {
+    assert.strictEqual(calculateNumber(-1.7, -2), -4);
+  });
 
 
 });
