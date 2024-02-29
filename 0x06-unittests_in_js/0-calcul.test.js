2cf18f1ec33d7f41735583b414d7f1af05a9d3ce
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-02-29 14:57:37 +0100

    😎😁😋 Task0 more test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -13,13 +13,11 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1.7, 2), 4);
   });
 
-  // all in negative
-  it('should return -4 when inputs are -2 and -2', () => {
-    assert.strictEqual(calculateNumber(-2, -2), -4);
+
+  //check5
+  it('should return -4 when inputs are -1.7 and -2', () => {
+    assert.strictEqual(calculateNumber(-1.7, -2), -4);
   });
-
-
-
 
 
 });
