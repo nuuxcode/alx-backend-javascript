c968487161a153ccd29d45db29c6959188a00023
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-03-01 03:59:18 +0100

    😋😉😉 Task9 Regex integration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,6 +2,14 @@ const request = require('request');
 const { expect } = require('chai');
 
 describe('Cart page', function () {
+  it('tests the server', function (done) {
+    request.get('http://localhost:7865', function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
   it('should return status code 200 when id is a number', function (done) {
     request.get('http://localhost:7865/cart/12', function (error, response, body) {
       expect(response.statusCode).to.equal(200);
@@ -15,3 +23,12 @@ describe('Cart page', function () {
       expect(response.statusCode).to.equal(404);
       done();
     });
+  });
+
+  it('tests the server with wrong cart id', function (done) {
+    request.get('http://localhost:7865/cart/wrong', function (error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
