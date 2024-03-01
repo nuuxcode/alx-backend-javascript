4b21a8772948ea4bf2416e1ac408ba4c393ac5b0
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-03-01 03:50:25 +0100

    😛😮🤨 Task8 testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,17 +1,17 @@
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api.js');
 
 describe('Index page', function () {
   it('should return status code 200', function (done) {
-    request('http://localhost:7865', function (error, response, body) {
+    request.get('http://localhost:7865', function (error, response, body) {
       expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
 
   it('should return correct result', function (done) {
-    request('http://localhost:7865', function (error, response, body) {
+    request.get('http://localhost:7865', function (error, response, body) {
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
